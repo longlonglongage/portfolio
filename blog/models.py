cd4c064516c952c33595f517907bc5914368453f
Coconut_Husk <1147583895@qq.com>
2019-04-30 13:43:15 +0800

    美化了Blog页面
    Blog主页只显示少量正文

--- blog/models.py
+++ blog/models.py
@@ -5,9 +5,12 @@ from django.db import models
 
 class Blog(models.Model):
     title = models.CharField(default='文章标题', max_length=50)
-    data = models.DateField()
+    date = models.DateField()
     image = models.ImageField(default='default.png', upload_to='images/')
     text = models.TextField(default='正文')
 
     def __str__(self):
         return self.title
+
+    def short_text(self):
+        return self.text[:60] + '......'
